a2c1fea872085fa3f82150d6721a8838c67dc304
dagume <guechadaniel@gmail.com>
2020-08-07 12:44:43 -0500

    challenge done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = 'https://rickandmortyapi.com/api/character/'; 
 
-const getData = api => {
-  fetch(api)
+window.addEventListener("beforeunload", () => {
+  localStorage.clear();
+});
+
+const  getData = async (api) => {
+  await fetch(api)
     .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
+    .then(response => {      
+      const characters = response.results
+      let output = characters
+        .map(character => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
+        <h2>${character.name}<span>${character.gender}</span></h2>
       </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
+      `;
+    })
+    .join("")
+
+  let newItem = document.createElement("section");
+      newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      //Enviar a localStorage el siguiente fetch
+      localStorage.setItem("next_fetch", response.info.next);
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = api => {
+  getData(api);
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
-  }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      let next_fetch_api = localStorage.getItem("next_fetch");
+      if (next_fetch_api == "") {
+        
+        return;
+      }
+
+      let api = next_fetch_api || API;
+      loadData(api);
+    }
+  },
+);
 
 intersectionObserver.observe($observe);
